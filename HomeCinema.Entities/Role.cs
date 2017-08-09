3c9072f97b7546899735fc2e28477069bf8f1d78
Neil Xie <neilxie001@gmail.com>
2017-08-09 12:46:58 -0400

    Create the Entity Models

--- HomeCinema.Entities/Role.cs
+++ HomeCinema.Entities/Role.cs
@@ -6,8 +6,9 @@ using System.Threading.Tasks;
 
 namespace HomeCinema.Entities
 {
-    public interface IEntityBase
+    public class Role : IEntityBase
     {
-        int ID { get; set; }
+        public int ID { get; set; }
+        public string Name { get; set; }
     }
 }
